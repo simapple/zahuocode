bfd16ff8a959fbb8a31ec259ef88a125b7b5ea1d
simapple <simapple@live.com>
2014-01-26 10:13:12 +0800

    check driver use php

--- check-drive-write.php
+++ check-drive-write.php
@@ -1,20 +1,25 @@
 <?php
+/*
+by simapple
+website http://www.simapple.com
+ */
+
 set_time_limit(0);
-$str =  "";
 $str = str_pad($str, 512, "0");
-echo strlen($str)."\n";
 $file = fopen("./temp","a+");
 $i = 0;
+$bytes = 1000000000;
+$mygabyte = $bytes/1000000;
+echo "test start. waiting ...";
 $starttime = time();
-$bytes = 10000000;
-$mygabyte = $bytes/1000;
 while($i < $bytes){
     $i += fwrite($file, $str);
 }
+
 $endtime = time();
 $usetime = $endtime - $starttime;
 $write_xiaolv = $bytes/$usetime;
 $myga_xiaolv = $mygabyte/$usetime;
 echo "create 1GB file use time ".$usetime." seconds\n";
-echo "xiaolv :".$write_xiaolv." byte/s\n";
-echo "xiaolv :".$myga_xiaolv." m/s\n";
+echo "speed :".$write_xiaolv." byte/s\n";
+echo "speed :".$myga_xiaolv." m/s\n";
